024a7fafb382e4d8f38909c103c9037151bf8a9f
douglas04 <msantosbrioso@alu.ufc.br>
2020-12-07 18:52:51 +0000

    Passou em todos testes.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
-
+c = float(input("Qual a temperatura em graus celsius?\n"))
+j = c * 9 / 5 + 32
+print(f"A temperatura em fahrenheit é {j}.")
 
 if __name__ == '__main__':
     main()
